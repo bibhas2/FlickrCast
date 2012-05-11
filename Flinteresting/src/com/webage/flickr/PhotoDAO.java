18b67998976884e1fdc821ead0fb952f25658788
bibhas2 <bibhas2@gmail.com>
2012-05-11 11:37:59 -0400

    NA

--- Flinteresting/src/com/webage/flickr/PhotoDAO.java
+++ Flinteresting/src/com/webage/flickr/PhotoDAO.java
@@ -3,8 +3,6 @@ package com.webage.flickr;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -12,7 +10,10 @@ import org.w3c.dom.NodeList;
 
 import android.content.Context;
 import android.preference.PreferenceManager;
+import android.util.DisplayMetrics;
+import android.view.WindowManager;
 
+import com.webage.util.Logger;
 import com.webage.util.XMLHttpRequestEventTarget;
 import com.webage.util.XmlHttpRequest;
 import com.webage.util.XmlHttpRequestImpl;
@@ -21,7 +22,6 @@ public class PhotoDAO {
 	private static final int DEFAULT_FETCH_COUNT = 100;
 	Delegate<ArrayList<Photo>> delegate;
 	XmlHttpRequest req = new XmlHttpRequestImpl();
-	Logger logger = Logger.getLogger("Flinteresting");
 	Context context = null;
 
 	public PhotoDAO() {
@@ -56,12 +56,15 @@ public class PhotoDAO {
 		int fetchCount = getPrefAsInt("fetch_count", DEFAULT_FETCH_COUNT);
 		this.delegate = d;
 		delegate.onLoadStart();
+		final String qualityFlag = computeQualityFlag();
+		
+		Logger.v("Quality flag: " + qualityFlag);
 		
 		req.open("get", "http://api.flickr.com/services/rest/?method=flickr.interestingness.getList&api_key=8fa47539dde7b426a299aa41da9b02fb&per_page=" + fetchCount, null, null);
 		req.setOnReadyStateChange(new XMLHttpRequestEventTarget() {
 			@Override
 			public void onLoad() {
-				logger.info("Done downloading.");
+				Logger.v("Done downloading.");
 				Document doc = req.getResponseXML();
 				NodeList list = doc.getElementsByTagName("photo");
 				ArrayList<Photo> photoList = new ArrayList<Photo>(500);
@@ -76,7 +79,12 @@ public class PhotoDAO {
 					String farm = e.getAttribute("farm");
 					String server = e.getAttribute("server");
 					String secret = e.getAttribute("secret");
-					String url = "http://farm" + farm + ".staticflickr.com/" + server + "/" + p.getId() + "_" + secret + ".jpg";
+					String url = "http://farm" + farm + ".staticflickr.com/" + server + "/" + p.getId() + "_" + secret;
+					if (qualityFlag != null) {
+						url = url + "_" + qualityFlag;
+					}
+					url += ".jpg";
+					
 					p.setUrl(url);
 					photoList.add(p);
 				}
@@ -87,12 +95,34 @@ public class PhotoDAO {
 
 			@Override
 			public void onError() {
-				logger.log(Level.SEVERE, "Error downloading image list", req.getError());
+				Logger.v("Error downloading image list", req.getError());
 				delegate.onError(req.getError());
 			}
 		});
 		req.send(null);
 		delegate.onLoadEnd();
+	}
+
+	private String computeQualityFlag() {
+		DisplayMetrics metrics = new DisplayMetrics();
+		WindowManager mgr = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+		mgr.getDefaultDisplay().getMetrics(metrics);
+		
+		int dimension = metrics.heightPixels > metrics.widthPixels ? metrics.heightPixels : metrics.widthPixels;
+		
+		String flagList[] = {"n", null, "z", "c", "b"};
+		int sizeList[] = {320, 500, 640, 800, 1024};
+		
+		//Return the smallest image size which is larger than the 
+		//device
+		for (int i = 0; i < sizeList.length; ++i) {
+			if (sizeList[i] >= dimension) {
+				return flagList[i];
+			}
+		}
+		
+		//return max image size
+		return flagList[sizeList.length - 1];
 	}
 
 }
